484fe07c99e71fb06564eda01102c1961b1f7168
Ryan <bugnir1@gmail.com>
2021-06-28 12:54:11 -0700

    added datetime to db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Task.new(title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in imperdiet lacus. Nulla sodales, tortor ac bibendum blandit, purus neque aliquet metus, vitae finibus ex felis sed urna. Fusce aliquet purus metus, vitae congue nulla pharetra eu. Nulla quis laoreet sem, at egestas dui.', completed: false, due_date: DateTime.new(2012, 8, 29, 22, 35, 0) )
-Task.new(title: 'Proin sit amet libero et libero consequat pharetra.', description: 'Nunc feugiat sollicitudin aliquet.', completed: false, due_date: '2021-06-29')
-Task.new(title: 'Etiam a diam vitae odio viverra varius.', description: 'Nulla gravida tincidunt velit, id ultricies neque mattis ac. Fusce mollis porta enim.', completed: false, due_date: '2021-06-29')
-Task.new(title: 'Duis consectetur risus sed vestibulum ultrices.', description: 'Nunc luctus pretium tincidunt.', completed: false, due_date: '2021-06-29')
-Task.new(title: 'Fusce consequat tortor sed tincidunt efficitur.', description: 'Suspendisse non ex molestie nunc imperdiet efficitur ut quis enim.', completed: false, due_date: '2021-06-29')
-Task.new(title: 'Fusce quis quam sit amet leo suscipit feugiat.', description: 'Maecenas a ante turpis. ', completed: false, due_date: '2021-06-29')
-Task.new(title: 'Fusce in purus venenatis, cursus nunc eget, aliquet libero.', description: 'Sed congue ultrices sapien nec vulputate.', completed: false, due_date: '2021-06-29')
+Task.create(title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in imperdiet lacus. Nulla sodales, tortor ac bibendum blandit, purus neque aliquet metus, vitae finibus ex felis sed urna. Fusce aliquet purus metus, vitae congue nulla pharetra eu. Nulla quis laoreet sem, at egestas dui.', completed: false, due_date: DateTime.new(2021, 8, 29, 22, 35, 0) )
+
+Task.create(title: 'Proin sit amet libero et libero consequat pharetra.', description: 'Nunc feugiat sollicitudin aliquet.', completed: true, due_date: DateTime.new(2021, 5, 29, 10, 35, 0))
+
+Task.create(title: 'Etiam a diam vitae odio viverra varius.', description: 'Nulla gravida tincidunt velit, id ultricies neque mattis ac. Fusce mollis porta enim.', completed: false, due_date: DateTime.new(2021, 7, 10, 12, 45, 0))
+
+Task.create(title: 'Duis consectetur risus sed vestibulum ultrices.', description: 'Nunc luctus pretium tincidunt.', completed: false, due_date: DateTime.new(2021, 4, 15, 10, 35, 0))
+
+Task.create(title: 'Fusce consequat tortor sed tincidunt efficitur.', description: 'Suspendisse non ex molestie nunc imperdiet efficitur ut quis enim.', completed: false, due_date: DateTime.new(2021, 9, 12, 10, 25, 0))
+
+Task.create(title: 'Fusce quis quam sit amet leo suscipit feugiat.', description: 'Maecenas a ante turpis. ', completed: true, due_date: DateTime.new(2021, 2, 12, 16, 10, 0))
+
+Task.create(title: 'Fusce in purus venenatis, cursus nunc eget, aliquet libero.', description: 'Sed congue ultrices sapien nec vulputate.', completed: false, due_date: DateTime.new(2021, 7, 12, 22, 35, 0))
 
 
