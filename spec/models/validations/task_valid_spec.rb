8a9b462424c19c524f6f7ca3f3b35b7c50f14cdc
Ryan <bugnir1@gmail.com>
2021-06-28 11:39:40 -0700

    created task model with validations and basic rspec tests

---
+++ spec/models/validations/task_valid_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe Task, type: :model do
+  describe 'validations' do
+    it 'ensures all required  fiels are present in order to save' do
+      task = Task.new
+      expect(task.valid?).to be(false)
+    end
+
+
+     it 'should save a valid record' do
+      task = Task.new(title: "Test Task", description: "description goes here", completed: false, due_date: '2021-06-29')
+      expect(task.valid?).to be(true)
+      task.save
+      expect(Task.count).to be(1)
+    end
+  end
+end
