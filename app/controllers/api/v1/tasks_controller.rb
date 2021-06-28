4cf64b1effe895446c2951172a8639461fd7a530
Ryan <bugnir1@gmail.com>
2021-06-28 14:24:33 -0700

    implmented creat task action with tests

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -12,6 +12,25 @@ module Api
         render json: TaskSerializer.new(task), status: 200
       end
 
+      def create
+        task = Task.new(create_task_params)
+
+        if task.save
+          render json: {message: 'Task successfully created.'}, status: 200
+        else
+          render json: {error: task.errors.messages}, status: 422
+        end
+
+
+      end
+
+      private
+
+      def create_task_params
+        params.require(:task).permit(:title, :description, :due_date, :completed)
+
+      end
+
     end
   end
 end
--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -12,12 +12,26 @@ RSpec.describe "Tasks", type: :request do
     end
   end
 
-    describe "GET /shot" do
+  describe "GET /tasks/:id" do
     it 'returns a json response with a specific task' do
       task = Task.create(title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in imperdiet lacus. Nulla sodales, tortor ac bibendum blandit, purus neque aliquet metus, vitae finibus ex felis sed urna. Fusce aliquet purus metus, vitae congue nulla pharetra eu. Nulla quis laoreet sem, at egestas dui.', completed: false, due_date: DateTime.new(2021, 8, 29, 22, 35, 0) )
       get "/api/v1/tasks/#{task.id}"
       expect(response).to have_http_status(200)
       expect(JSON.parse(response.body)['data']['type']).to eq('task')
+    end
+  end
+
+  describe "POST /tasks" do
+    it 'creates a new task if it is valid' do
+      post "/api/v1/tasks", params: {task: {title: 'Test Task', description: 'Test description', completed: true, due_date: DateTime.new}}
+      expect(response).to have_http_status(200)
+      expect(JSON.parse(response.body)).to eq("message" => "Task successfully created.")
+    end
+
+    it 'returns an error if the task is not valid' do
+      post "/api/v1/tasks", params: {task: {title: 'Test Task'}}
+      expect(response).to have_http_status(422)
+      expect(JSON.parse(response.body)).to eq({"error"=>{"description"=>["can't be blank"], "due_date"=>["can't be blank"]}})
     end
   end
 end
