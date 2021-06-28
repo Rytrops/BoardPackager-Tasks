75ea0794ae7546c993404c357caf2d6fd616598d
Ryan <bugnir1@gmail.com>
2021-06-28 13:10:56 -0700

    implemented index method in tasks w/ tests

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -3,7 +3,8 @@ module Api
     class TasksController < ApplicationController
 
       def index
-
+        tasks = Task.all
+        render json: TaskSerializer.new(tasks), status: 200
       end
     end
   end
--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "Tasks", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'returns a json response with all tasks' do
+      Task.create(title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in imperdiet lacus. Nulla sodales, tortor ac bibendum blandit, purus neque aliquet metus, vitae finibus ex felis sed urna. Fusce aliquet purus metus, vitae congue nulla pharetra eu. Nulla quis laoreet sem, at egestas dui.', completed: false, due_date: DateTime.new(2021, 8, 29, 22, 35, 0) )
+      Task.create(title: 'Proin sit amet libero et libero consequat pharetra.', description: 'Nunc feugiat sollicitudin aliquet.', completed: true, due_date: DateTime.new(2021, 5, 29, 10, 35, 0))
+      get '/api/v1/tasks'
+      expect(response).to have_http_status(200)
+      expect(JSON.parse(response.body)['data'].length).to eq(2)
+      expect(JSON.parse(response.body)['data'].first['type']).to eq('task')
+    end
   end
 end
