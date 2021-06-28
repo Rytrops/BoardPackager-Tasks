fa44a4c6ede7111339e3f4ab1fceb3853bb2608b
Ryan <bugnir1@gmail.com>
2021-06-28 13:25:13 -0700

    added show task action and tests

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -6,6 +6,12 @@ module Api
         tasks = Task.all
         render json: TaskSerializer.new(tasks), status: 200
       end
+
+      def show
+        task = Task.find_by(id: params[:id])
+        render json: TaskSerializer.new(task), status: 200
+      end
+
     end
   end
 end
--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -11,4 +11,13 @@ RSpec.describe "Tasks", type: :request do
       expect(JSON.parse(response.body)['data'].first['type']).to eq('task')
     end
   end
+
+    describe "GET /shot" do
+    it 'returns a json response with a specific task' do
+      task = Task.create(title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque in imperdiet lacus. Nulla sodales, tortor ac bibendum blandit, purus neque aliquet metus, vitae finibus ex felis sed urna. Fusce aliquet purus metus, vitae congue nulla pharetra eu. Nulla quis laoreet sem, at egestas dui.', completed: false, due_date: DateTime.new(2021, 8, 29, 22, 35, 0) )
+      get "/api/v1/tasks/#{task.id}"
+      expect(response).to have_http_status(200)
+      expect(JSON.parse(response.body)['data']['type']).to eq('task')
+    end
+  end
 end
