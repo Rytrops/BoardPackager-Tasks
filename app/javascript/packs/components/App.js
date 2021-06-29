631d2cbcd7307dec9d9aa0354dd9dd85d523556b
Ryan <bugnir1@gmail.com>
2021-06-29 12:22:45 -0700

    added navbar for easier navigation

--- app/javascript/packs/components/App.js
+++ app/javascript/packs/components/App.js
@@ -4,11 +4,13 @@ import CreateTask from './tasks/CreateTask';
 import EditTask from './tasks/EditTask';
 import Tasks from './tasks/Tasks';
 import TaskShow from './tasks/TaskShow';
+import NaviBar from './tasks/NaviBar';
 
 const App = () => {
   return (
     <div className='bg-light vh-100'>
       <Router>
+        <NaviBar />
         <Switch>
           <Route exact path='/' component={Tasks} />
           <Route exact path='/tasks/:id' component={TaskShow} />
---
+++ app/javascript/packs/components/tasks/NaviBar.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import {
+  Navbar,
+  Nav,
+  Button,
+  NavDropdown,
+  DropdownButton,
+} from 'react-bootstrap';
+import { useHistory } from 'react-router-dom';
+
+const NaviBar = () => {
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventdefault;
+    localStorage.clear();
+    history.push('/');
+    window.location.reload(false);
+  };
+
+  const handleClick = (path, e) => {
+    history.push(path);
+  };
+  return (
+    <Navbar bg='light' expand='lg'>
+      <Button
+        type='button'
+        className='mr-2'
+        variant='secondary'
+        size='sm'
+        onClick={(e) => {
+          handleClick('/');
+        }}
+      >
+        Tasks List
+      </Button>
+      <Button
+        type='button'
+        className=''
+        variant='secondary'
+        size='sm'
+        onClick={(e) => {
+          handleClick('/create');
+        }}
+      >
+        Create New Task
+      </Button>
+    </Navbar>
+  );
+};
+
+export default NaviBar;
