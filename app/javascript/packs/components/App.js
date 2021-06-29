e7337cc4a3ff9fb4eee28c0ae208f48791b86678
Ryan <bugnir1@gmail.com>
2021-06-29 09:51:22 -0700

    user can add a task, has form validation and error messaging

--- app/javascript/packs/components/App.js
+++ app/javascript/packs/components/App.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
-  Link,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import CreateTask from './tasks/CreateTask';
 import Tasks from './tasks/Tasks';
 import TaskShow from './tasks/TaskShow';
 
@@ -16,6 +11,7 @@ const App = () => {
         <Switch>
           <Route exact path='/' component={Tasks} />
           <Route exact path='/tasks/:id' component={TaskShow} />
+          <Route exact path='/create' component={CreateTask} />
         </Switch>
       </Router>
     </div>
